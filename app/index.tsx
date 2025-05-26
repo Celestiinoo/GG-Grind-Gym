ad3f389a6c7420cd52dd139407b13b54f8424b5c
João Felipe <jaofelipes22@gmail.com>
2025-05-26 17:15:06 -0400

    login page

--- app/index.tsx
+++ app/index.tsx
@@ -12,10 +12,21 @@ import {
 } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 
+
+
 export default function Index() {
 	return (
-		<View className="flex-1 items-center">
-            <Text className="text-2xl font-bold">PRIMEIRA PAGINA HEIN!</Text>
+		<View className="bg-gradient-to-tr from-[#f76e26] to-[#9d39d5] flex-1">
+			<View className="flex-1 items-center justify-center">
+            	<Text className="text-2xl font-bold"></Text>
+				<Image source={require("./(fotos)/vector2.jpg")} className="rounded m-24" 
+				style={{
+					height: 200,
+					width: 200,
+					resizeMode: "contain",
+				}}
+				></Image>
+			</View>
 		</View>
 	);
 }
